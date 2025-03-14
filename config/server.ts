23f3a7eb68ca6da73220115215ffcc7c90551a73
phelipe-gh <batistaphelipe153@gmail.com>
2025-03-13 22:51:23 -0300

    Alterando envs

--- config/server.ts
+++ config/server.ts
@@ -1,12 +1,18 @@
 export default ({ env }) => {
   // Imprime "phelipe" no log
-  console.log('phelipe');
-  console.log('phelipe ' + process.env.TESTE_T);
+  console.log('phelipe1 ' + process.env.TESTE_T);
+  console.log('phelipe2 ' + env('TESTE_T'));
+  
   return {
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     app: {
-      keys: env('APP_KEYS', '').split(','), // Converte a variável APP_KEYS para um array de chaves.
+      keys: [
+        '23U8nm7HILiV7349ZLHCTA==',
+        'wRyvSH+9R+2aiKQrz5fknw==',
+        '9qajpTG7Icfb+Lywd3IdHg==',
+        'NO8c99QL8i8q7hKea9Ymmg=='
+      ]      
     },
   };
 };
