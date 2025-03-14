296c4d4a1f73d3358388552e81224d68b87c1c02
phelipe-gh <batistaphelipe153@gmail.com>
2025-03-13 23:02:03 -0300

    Alterando envs

--- config/admin.ts
+++ config/admin.ts
@@ -1,17 +1,17 @@
-export default ({ env }) => ({
+export default () => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: 'I6lql03wDItwvu0VLmlz/w==', // ADMIN_JWT_SECRET
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: '5I4qQu8rj0vCZtVwSvVH4A==', // API_TOKEN_SALT
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: 'tkhGYXZ54tF7KCtnvLDgBg==', // TRANSFER_TOKEN_SALT
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: true, // FLAG_NPS
+    promoteEE: true, // FLAG_PROMOTE_EE
   },
 });
